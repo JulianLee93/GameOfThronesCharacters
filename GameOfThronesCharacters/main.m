c995b247b55705ed6c8f1d724c8bcd412096802c
Julian Lee <Julian@julians-macbook-pro.local>
2016-01-26 13:47:04 -0800

    Initial Commit

---
+++ GameOfThronesCharacters/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GameOfThronesCharacters
+//
+//  Created by Julian Lee on 1/26/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
