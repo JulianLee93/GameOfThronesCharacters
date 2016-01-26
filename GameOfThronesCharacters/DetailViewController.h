b6b09d8db7d9277aa3b4fa493c88511217b621e2
andymill <andymill@gmail.com>
2016-01-26 14:58:15 -0800

    did add detail viewcontroller

---
+++ GameOfThronesCharacters/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  GameOfThronesCharacters
+//
+//  Created by Andrew Miller on 1/26/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
